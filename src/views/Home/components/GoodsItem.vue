09eeadd55ca287a414fa4400c1c06aac1f69905e
kelseysl <kelseysl320@gmail.com>
2025-08-01 01:23:33 +0800

    Home-GoodsItem组件封装

---
+++ src/views/Home/components/GoodsItem.vue
@@ -0,0 +1,55 @@
+<script setup>
+defineProps({
+  goods:{
+    type: Object,
+    default: () => ({})//没有传递property时，默认值为一个空对象
+  }
+});
+</script>
+
+<template>
+  <RouterLink to="/" class="goods-item">
+    <img v-img-lazy="goods.picture" alt="" />
+    <p class="name ellipsis">{{ goods.name }}</p>
+    <p class="desc ellipsis">{{ goods.desc }}</p>
+    <p class="price">&yen;{{ goods.price }}</p>
+    </RouterLink>
+</template>
+
+<style scoped lang='scss'>
+  .goods-item {
+      display: block;
+      width: 220px;
+      padding: 20px 30px;
+      text-align: center;
+      transition: all .5s;//过渡效果
+
+      &:hover {
+        transform: translate3d(0, -3px, 0);// 悬浮时向上轻微移动，并添加阴影，产生“浮起来”的立体感
+        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+      }
+
+      img {
+        width: 160px;
+        height: 160px;
+      }
+
+      p {
+        padding-top: 10px;
+      }
+
+      .name {
+        font-size: 16px;
+      }
+
+      .desc {
+        color: #999;
+        height: 29px;
+      }
+
+      .price {
+        color: $priceColor;
+        font-size: 20px;
+      }
+    }
+</style>
--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,6 +2,7 @@
 import HomePanel from './HomePanel.vue'
 import { getGoodsAPI } from '@/apis/home';
 import { ref,onMounted } from "vue";
+import GoodsItem from './GoodsItem.vue';
 
 //获取数据列表
 const goodsProduct = ref([]);
@@ -26,12 +27,7 @@ onMounted(() => getGoods())
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodsItem :goods="good" />
           </li>
         </ul>
       </div>
